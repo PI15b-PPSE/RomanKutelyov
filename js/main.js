0780e7c83d9823bb0ecdcffdb332e5ef82b00350
SkyLiTeR <Revento23@yandex.ru>
2018-03-06 07:33:16 +0200

    Add files via upload
    Изменена структура игры, код javascript вынесен в отдельный файл.

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var audio = document.getElementById("myaudio");
+audio.volume = 0.1;
+
+var image_tank = 'image/tank-up.png'; 
+
+//элемент отрисовки
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+//Даннные игрока
+var x = canvas.width/2;
+var y = canvas.height/2;
+var speed=10;
+
+
+//функция обработки клавиш
+function key_Pressed(e){
+    switch(e.keyCode){
+         
+        case 37:  // если нажата клавиша влево           
+            x = x - speed; 
+            image_tank = 'image/tank-left.png'; 
+            break;
+        case 38:   // если нажата клавиша вверх
+            y = y - speed;
+            image_tank = 'image/tank-up.png'; 
+            break;
+        case 39:   // если нажата клавиша вправо
+            x = x + speed;
+            image_tank = 'image/tank-right.png'; 
+            break;
+        case 40:   // если нажата клавиша вниз
+             y = y + speed;
+             image_tank = 'image/tank-down.png'; 
+            break;
+    }
+}
+ 
+addEventListener("keydown", key_Pressed);
+
+
+
+//Функция отрисовки игрока
+function drawTank() {
+	var example = document.getElementById("myCanvas");
+    example.width = window.innerWidth;
+    example.height = window.innerHeight;
+    ctx = example.getContext('2d'); 
+    ctx.fillRect(0, 0, screen.width, screen.height)
+    pic = new Image();             
+    pic.src = image_tank;  
+    pic.onload = function() {    
+    ctx.drawImage(pic, x, y);  
+    };
+}
+
+function Render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawTank();
+}
+
+setInterval(Render, 10);
