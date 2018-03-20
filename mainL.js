0040318c86bbc4d88164a03c8fdf7f17fed4cefd
SkyLiTeR <Revento23@yandex.ru>
2018-03-20 07:15:19 +0200

    Добавлен обработчик нажатия клавиш.

--- mainL.js
+++ mainL.js
@@ -15,6 +15,31 @@ var ctx = canvas.getContext("2d");
 //Даннные игрока
 var x = canvas.width/2;
 var y = canvas.height/2;
+var speed=10;
+//функция обработки клавиш
+function key_Pressed(e){
+    switch(e.keyCode){
+         
+        case 37:  // если нажата клавиша влево           
+            x = x - speed; 
+            imagetank = 'image/tank-left.png'; 
+            break;
+        case 38:   // если нажата клавиша вверх
+            y = y - speed;
+            imagetank = 'image/tank-up.png'; 
+            break;
+        case 39:   // если нажата клавиша вправо
+            x = x + speed;
+            imagetank = 'image/tank-right.png'; 
+            break;
+        case 40:   // если нажата клавиша вниз
+             y = y + speed;
+             imagetank = 'image/tank-down.png'; 
+            break;
+    }
+}
+ 
+addEventListener("keydown", key_Pressed);
 //Функция отрисовки игрока
 function drawTank() {
 	var example = document.getElementById("myCanvas");
@@ -23,7 +48,7 @@ function drawTank() {
     ctx = example.getContext('2d'); 
     ctx.fillRect(0, 0, screen.width, screen.height)
     pic = new Image();             
-    pic.src = image_tank;  
+    pic.src = imagetank;  
     pic.onload = function() {    
     ctx.drawImage(pic, x, y);  
     };
